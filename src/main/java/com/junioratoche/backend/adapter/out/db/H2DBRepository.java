f0d454081249d706f47a4e36acd91b2f62b7daa3
Junior Atoche <gjquisuruco@indracompany.com>
2023-11-25 11:19:41 -0500

    perf(query): add filter the query BD

--- src/main/java/com/junioratoche/backend/adapter/out/db/H2DBRepository.java
+++ src/main/java/com/junioratoche/backend/adapter/out/db/H2DBRepository.java
@@ -11,6 +11,7 @@ import com.junioratoche.backend.domain.Price;
 import com.junioratoche.backend.port.out.db.EntityRepositoryOutputPort;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 
@@ -32,17 +33,23 @@ public class H2DBRepository implements EntityRepositoryOutputPort {
 
 	@Override
 	public Price getPriceByBrandAndProductInApplicationDate(LocalDateTime applicationDate, int productId, int brandId) {
-		TypedQuery<PriceEntity> query = entityManager.createQuery("SELECT p FROM PriceEntity p "
-				+ "WHERE p.startDate <= :applicationDate " + "AND p.endDate >= :applicationDate "
-				+ "AND p.productId = :productId " + "AND p.brand.id = :brandId " + "ORDER BY p.priority DESC",
-				PriceEntity.class);
+	    TypedQuery<PriceEntity> query = entityManager.createQuery(
+	        "SELECT p FROM PriceEntity p " +
+	        "WHERE :applicationDate BETWEEN p.startDate AND p.endDate " +
+	        "AND p.productId = :productId AND p.brand.id = :brandId " +
+	        "ORDER BY p.priority DESC", PriceEntity.class);
 
-		query.setParameter("applicationDate", applicationDate);
-		query.setParameter("productId", productId);
-		query.setParameter("brandId", brandId);
-		query.setMaxResults(1);
+	    query.setParameter("applicationDate", applicationDate);
+	    query.setParameter("productId", productId);
+	    query.setParameter("brandId", brandId);
+	    query.setMaxResults(1);
 
-		List<PriceEntity> resultList = query.getResultList();
-		return resultList.isEmpty() ? null : priceEntityMapper.priceEntityToPrice(resultList.get(0));
+	    try {
+	        PriceEntity result = query.getSingleResult();
+	        return priceEntityMapper.priceEntityToPrice(result);
+	    } catch (NoResultException e) {
+	        return null;
+	    }
 	}
+
 }
